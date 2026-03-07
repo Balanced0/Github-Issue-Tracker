acf1a3da58768dac249fcdeb5efd83210290f495
balanced0 <alvichowdhury013@gmail.com>
2026-03-07 22:33:47 +0600

    Added Sign In functionality

---
+++ script/index.js
@@ -0,0 +1,54 @@
+document.getElementById("signIn-btn").addEventListener("click", () => {
+    const usernameId = document.getElementById("username");
+    const username = usernameId.value;
+    const passwordId = document.getElementById("password");
+    const password = passwordId.value;
+    if(!username && !password){
+        const alertSection = document.getElementById("alert-container");
+        alertSection.innerHTML = `
+            <div role="alert" class="alert alert-error">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>Invalid username or password</span>
+            </div>
+        `;
+    }
+    if(username !== "admin"){
+        const alertSection = document.getElementById("alert-container");
+        alertSection.innerHTML = `
+            <div role="alert" class="alert alert-error">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>Invalid username</span>
+            </div>
+        `;
+        return;
+    }
+    else if(password !== "admin123"){
+        const alertSection = document.getElementById("alert-container");
+        alertSection.innerHTML = `
+            <div role="alert" class="alert alert-error">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>Invalid password</span>
+            </div>
+        `;
+        return;
+    }
+    else{
+        const alertSection = document.getElementById("alert-container");
+        alertSection.innerHTML = `
+            <div role="alert" class="alert alert-success">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>Sign In successful!</span>
+            </div>
+        `;
+        window.location.href = "home.html";
+        return;
+    }
+})
